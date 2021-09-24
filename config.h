88c99f3065e0800fbb1d7be44917139f0870cfc8
Samuel Kunst <samuel.kunst@gmail.com>
2021-09-24 15:16:37 +0200

    add keybinding for password script

--- config.h
+++ config.h
@@ -71,7 +71,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *browsercmd[]  = { "chromium", NULL };
-
+static const char *dmenu_pass[] = { "dmenu-pass", NULL };
 /* Xresources preferences to load at startup */
 ResourcePref resources[] = {
   { "font",            STRING,  &font },
@@ -96,6 +96,7 @@ static Key keys[] = {
   { MODKEY,            XK_p,      spawn,             {.v = dmenucmd } },
   { MODKEY|ShiftMask,  XK_Return, spawn,             {.v = termcmd } },
   { MODKEY,            XK_w,      spawn,             {.v = browsercmd } },
+  { MODKEY|ShiftMask,  XK_p,      spawn,             {.v = dmenu_pass } },
   { MODKEY,            XK_b,      togglebar,         {0} },
   { MODKEY,            XK_j,      focusstack,        {.i = +1 } },
   { MODKEY,            XK_k,      focusstack,        {.i = -1 } },
