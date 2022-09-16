559fe2eb1d9845c9b1e2d2d923a5f3961999599b
Samuel Kunst <samuel.kunst@gmail.com>
2022-09-16 16:42:51 +0200

    feat: add xresources patch

--- config.h
+++ config.h
@@ -1,22 +1,23 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
+static unsigned int borderpx  = 2;        /* border pixel of windows */
+static unsigned int snap      = 32;       /* snap pixel */
+static int showbar            = 1;        /* 0 means no bar */
+static int topbar             = 1;        /* 0 means bottom bar */
 static char font[]                = "JetBrains Mono:pixelsize=14:antialias=true:autohint=true";
 static char dmenufont[]           = "JetBrains Mono:pixelsize=14:antialias=true:autohint=true";
 static const char *fonts[]        = { font, "JoyPixels:pixelsize=12:antialias=true:autohint=true"};
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+static char normbgcolor[]           = "#222222";
+static char normbordercolor[]       = "#444444";
+static char normfgcolor[]           = "#bbbbbb";
+static char selfgcolor[]            = "#eeeeee";
+static char selbordercolor[]        = "#005577";
+static char selbgcolor[]            = "#005577";
+static char *colors[][3] = {
+  /*               fg           bg           border   */
+  [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
+  [SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor  },
 };
 
 /* tagging */
@@ -33,9 +34,9 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact     = 0.5; /* factor of master area size [0.05..0.95] */
-static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static float mfact     = 0.5; /* factor of master area size [0.05..0.95] */
+static int nmaster     = 1;    /* number of clients in master area */
+static int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
 static const Layout layouts[] = {
@@ -57,12 +58,33 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]  = { "kitty", NULL };
 static const char *browsercmd[]  = { "chromium", NULL };
 
 /* forward define shiftview function */
 void shiftview(const Arg *arg);
+
+/*
+ * Xresources preferences to load at startup
+ */
+ResourcePref resources[] = {
+  { "font",            STRING,  &font },
+  { "font",            STRING,  &dmenufont },
+  { "color0",          STRING,  &normbgcolor },
+  { "color0",          STRING,  &normbordercolor },
+  { "color4",          STRING,  &normfgcolor },
+  { "color4",          STRING,  &selbgcolor },
+  { "color8",          STRING,  &selbordercolor },
+  { "color0",          STRING,  &selfgcolor },
+  { "borderpx",        INTEGER, &borderpx },
+  { "snap",            INTEGER, &snap },
+  { "showbar",         INTEGER, &showbar },
+  { "topbar",          INTEGER, &topbar },
+  { "nmaster",         INTEGER, &nmaster },
+  { "resizehints",     INTEGER, &resizehints },
+  { "mfact",           FLOAT,   &mfact },
+};
 
 static const Key keys[] = {
 	/* modifier          key        function        argument */
