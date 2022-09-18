b0e27383cfbf4f1d0e9f234e33b2bd2fb1378687
Samuel Kunst <samuel.kunst@gmail.com>
2022-09-18 16:38:02 +0200

    fix: perform font-overrides for alternate devices

--- statusbar/launch.sh
+++ statusbar/launch.sh
@@ -13,5 +13,17 @@ killall -q polybar
 TMPFILE=$(mktemp /tmp/polybar.config.XXXXXX)
 sed "s/colors-default/$THEME/" "$CONFIG" > "$TMPFILE"
 
+# if running onn laptop perform additional modifications to config
+if [[ -f /sys/devices/virtual/dmi/id/chassis_vendor ]]; then
+  VENDOR=$(cat /sys/devices/virtual/dmi/id/chassis_vendor)
+  if [[ "$VENDOR" == "LENOVO" ]]; then
+    sed -i \
+      -e "s/height=.*/height=40/" \
+      -e "s/JetBrains Mono:pixelsize=13;3/JetBrains Mono:pixelsize=20;3/" \
+      -e "s/Hack Nerd Font Mono:size=17;3/Hack Nerd Font Mono:size=28;6/" \
+      "$TMPFILE"
+  fi
+fi
+
 # launch polybar
 polybar -c "$TMPFILE" "$BAR_NAME" 2>&1 | tee -a "$LOG_FILE" & disown
