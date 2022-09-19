4278c66b7db50b40545d131f162ca0cf24ddbda6
Samuel Kunst <samuel.kunst@gmail.com>
2022-09-19 17:57:05 +0200

    refactor: improve conditional injecting of hidpi fonts

--- statusbar/launch.sh
+++ statusbar/launch.sh
@@ -1,29 +1,42 @@
 #!/bin/bash
 
+# various constants & helper functions
 LOG_FILE="/tmp/polybar.log"
 CONFIG="$HOME"/.config/dwm/statusbar/bar.ini
 BAR_NAME=main
 THEME=${THEME:-colors-default}
 echo "$THEME"
-
-# kill polybar if already running
-killall -q polybar
-
-# make temporary polybar config with inserted theme
 TMPFILE=$(mktemp /tmp/polybar.config.XXXXXX)
-sed "s/colors-default/$THEME/" "$CONFIG" > "$TMPFILE"
 
-# if running onn laptop perform additional modifications to config
-if [[ -f /sys/devices/virtual/dmi/id/chassis_vendor ]]; then
-  VENDOR=$(cat /sys/devices/virtual/dmi/id/chassis_vendor)
-  if [[ "$VENDOR" == "LENOVO" ]]; then
-    sed -i \
-      -e "s/height=.*/height=40/" \
-      -e "s/JetBrains Mono:pixelsize=13;3/JetBrains Mono:pixelsize=20;3/" \
-      -e "s/Hack Nerd Font Mono:size=17;3/Hack Nerd Font Mono:size=28;6/" \
-      "$TMPFILE"
-  fi
-fi
+is_hidpi() {
+  # default to no hidpi if can't determine product name
+  [[ ! -f /sys/devices/virtual/dmi/id/product_name ]] && return 1
+  case $(cat /sys/devices/virtual/dmi/id/product_name) in
+    # add more product types here
+    "21CBCTO1WW")
+      return 0
+      ;;
+    *)
+      return 1
+      ;;
+  esac
+}
 
-# launch polybar
-polybar -c "$TMPFILE" "$BAR_NAME" 2>&1 | tee -a "$LOG_FILE" & disown
+set_theme() {
+  sed -i "s/colors-default/$THEME/" "$TMPFILE"
+}
+
+configure_hidpi() {
+  sed -i "s/fonts-default/fonts-hidpi/" "$TMPFILE"
+  sed -i "s/height=.*/height=40/" "$TMPFILE"
+}
+
+main() {
+  killall -q polybar
+  cat "$CONFIG" > "$TMPFILE"
+  set_theme
+  is_hidpi && configure_hidpi
+  polybar -c "$TMPFILE" "$BAR_NAME" 2>&1 | tee -a "$LOG_FILE" & disown
+}
+
+main
