547b48a904637a4e5c2f88d4abf3e5a52a20ebed
Samuel Kunst <samuel.kunst@gmail.com>
2022-10-07 18:54:26 +0200

    feat: allow various module layouts per-system

--- statusbar/launch.sh
+++ statusbar/launch.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 # various constants & helper functions
-LOG_FILE="/tmp/polybar.log"
-CONFIG="$HOME"/.config/dwm/statusbar/bar.ini
-BAR_NAME=main
-THEME=${THEME:-colors-default}
-echo "$THEME"
-TMPFILE=$(mktemp /tmp/polybar.config.XXXXXX)
+CONF_BASE="$HOME"/.config/dwm/statusbar
+TEMP_FILE=/tmp/polybar.config
+
+# environment variables injected into 'bar-base.ini'
+# these default to a non-hidpi, non-laptop bar configuration.
+export THEME_PATH="$CONF_BASE"/themes/colors-default.ini
+export MODULE_PATH="$CONF_BASE"/module-layouts/default.ini
+export FONT_PATH="$CONF_BASE"/font-config/fonts-default.ini
 
 is_hidpi() {
   # default to no hidpi if can't determine product name
@@ -23,20 +25,25 @@ is_hidpi() {
 }
 
 set_theme() {
-  sed -i "s/colors-default/$THEME/" "$TMPFILE"
+  sed -i "s/colors-default/$THEME/" "$TEMP_FILE"
 }
 
 configure_hidpi() {
-  sed -i "s/fonts-default/fonts-hidpi/" "$TMPFILE"
-  sed -i "s/height=.*/height=40/" "$TMPFILE"
+  sed -i "s/fonts-default/fonts-hidpi/" "$TEMP_FILE"
+  sed -i "s/height=.*/height=40/" "$TEMP_FILE"
 }
 
 main() {
+  # ensure polybar insance is not already running
   killall -q polybar
-  cat "$CONFIG" > "$TMPFILE"
-  set_theme
-  is_hidpi && configure_hidpi
-  polybar -c "$TMPFILE" "$BAR_NAME" 2>&1 | tee -a "$LOG_FILE" & disown
+
+  if is_hidpi; then
+    export MODULE_PATH="$CONF_BASE"/module-layouts/laptop.ini
+    export FONT_PATH="$CONF_BASE"/font-config/fonts-hidpi.ini
+  fi
+
+  envsubst < "$CONF_BASE"/bar-base.ini > "$TEMP_FILE"
+  polybar -c "$TEMP_FILE" main 2>&1 | tee -a /tmp/polybar.log & disown
 }
 
 main
